327cb4dc27c4bf8955280ca906c586e4d802012f
wilpet4 <william.petrik@gmail.com>
2022-06-28 17:50:47 +0200

    ScreeningModel genres don't work

--- AspNetCoreBerrasBio/BerrasBio/Models/ScreeningModel.cs
+++ AspNetCoreBerrasBio/BerrasBio/Models/ScreeningModel.cs
@@ -4,12 +4,16 @@ namespace BerrasBio.Models
 {
     public class ScreeningModel
     {
+        public ScreeningModel()
+        {
+            Genres = new List<Genre>();
+        }
         public int ScreeningId { get; set; }
         public string MovieName { get; set; }
         public string Description { get; set; }
         public int Runtime { get; set; }
         public int AgeRestriction { get; set; }
-        public List<string> Genres { get; set; }
+        public List<Genre> Genres { get; set; }
         public string DirectorName { get; set; }
     }
 }
